b9e698244b35c00c032ba3acd81154d2d9839b1e
Khalyomede <khalyomede@gmail.com>
2019-08-15 12:49:12 +0200

    removed node-sass-tilde-importer node package, added development build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,15 @@
 import { src, dest, watch, series, parallel } from "gulp";
 import pug from "gulp-pug";
 import sass from "gulp-sass";
-import tildeImporter from "node-sass-tilde-importer";
 import autoprefixer from "gulp-autoprefixer";
 import cleanCss from "gulp-clean-css";
 import browserify from "gulp-bro";
 import rename from "gulp-rename";
 import clean from "gulp-clean";
 import plumber from "gulp-plumber";
+import browserSync from "browser-sync";
+
+const browser = browserSync.create();
 
 const docsJs = () =>
 	src("src/docs/js/index.js")
@@ -29,13 +31,15 @@ const docsJs = () =>
 			})
 		)
 		.pipe(dest("docs/js"))
-		.pipe(dest("docs/material-components-web-vue/js"));
+		.pipe(dest("docs/material-components-web-vue/js"))
+		.pipe(browser.stream());
 
 const docsHtml = () =>
-	src("src/docs/index.pug")
+	src("src/docs/html/index.pug")
 		.pipe(plumber())
 		.pipe(pug())
-		.pipe(dest("docs"));
+		.pipe(dest("docs"))
+		.pipe(browser.stream());
 
 const docsCss = () =>
 	src("src/docs/css/index.sass")
@@ -53,17 +57,20 @@ const docsCss = () =>
 			})
 		)
 		.pipe(dest("docs/css"))
-		.pipe(dest("docs/material-components-web-vue/css"));
+		.pipe(dest("docs/material-components-web-vue/css"))
+		.pipe(browser.stream());
 
 const libCss = () =>
 	src("src/css/*.sass")
 		.pipe(plumber())
-		.pipe(dest("lib/css"));
+		.pipe(dest("lib/css"))
+		.pipe(browser.stream());
 
 const libJs = () =>
 	src("src/js/**/*.{vue,js}")
 		.pipe(plumber())
-		.pipe(dest("lib/js"));
+		.pipe(dest("lib/js"))
+		.pipe(browser.stream());
 
 const clearDocs = () =>
 	src("docs", { allowEmpty: true })
@@ -75,6 +82,26 @@ const clearLib = () =>
 		.pipe(plumber())
 		.pipe(clean());
 
+const reload = done => {
+	browser.reload();
+
+	done();
+};
+
 const build = series(clearDocs, clearLib, parallel(libCss, libJs), parallel(docsJs, docsHtml, docsCss));
 
-export { build };
+const start = () => {
+	browser.init({
+		server: {
+			baseDir: "docs"
+		}
+	});
+
+	watch("src/js/**/*.{vue,js}", series(libJs, docsJs, reload));
+	watch("src/docs/js/**/*.{vue,js}", series(docsJs, reload));
+	watch("src/css/**/*.sass", series(libCss, docsCss, reload));
+	watch("src/docs/css/**/*.sass", series(docsCss, reload));
+	watch("src/docs/html/**/*.pug", series(docsHtml, reload));
+};
+
+export { start, build };
